490dfdb7250c47eb7c2601161676803847414007
Nikita Kalachev <nikitakalachev18@gmail.com>
2017-09-11 15:18:46 +0400

    mst-slider. font-awesome

--- build/js/main.js
+++ build/js/main.js
@@ -1,4 +1,10 @@
-		<script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
+		$(".slider-img").click(function(){
+			$("#bigImage").attr('src', 
+				$(this).attr('src'));
+		});
+
+
+<script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
 	</head>
 	<body>
 
